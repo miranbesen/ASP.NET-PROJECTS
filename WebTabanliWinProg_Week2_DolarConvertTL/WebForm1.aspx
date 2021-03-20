12c70ea9c74770fbb5cde5fbadf42c5c3d3ec958
miranbesen <57726541+miranbesen@users.noreply.github.com>
2021-03-20 21:56:44 +0300

    Add files via upload

---
+++ WebTabanliWinProg_Week2_DolarConvertTL/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebTabanliWinProg_Week2_DolarConvertTL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Amout in US Dollars:
+            <asp:TextBox ID="TextBox1" runat="server" Height="25px" Width="168px"></asp:TextBox>
+     <asp:Button ID="Button1" runat="server" Height="31px" OnClick="Button1_Click" style="margin-left: 27px; margin-top: 2px; margin-bottom: 0px" Text="Convert to TL" OnClientClick="return confirm(&quot;Are you sure you really want to convert value to Turkish Liras?&quot;) " />
+        </div>
+        <p>
+&nbsp;<asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebTabanliWinProg_Week2_DolarConvertTL/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebTabanliWinProg_Week2_DolarConvertTL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Server.MapPath("Default.aspx");
+
+            Label2.BorderStyle = BorderStyle.Outset;
+            if(IsPostBack)
+            {
+                Label2.Text = "The page has been requested after a post back.";
+            }
+            else
+            {
+                TextBox1.Text = "0,00";
+                Label2.Text="The page has been requested by the GET method";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dollars = double.Parse(TextBox1.Text);
+                double tl = dollars * 7.40;
+
+                Label3.Text = dollars.ToString() + "US dollars equal to" + tl.ToString("F2") + "TL";
+            }
+            catch(Exception ex)
+            {
+                Label3.Text = "the following error has occured:<br /> <br />" + ex.Message;
+
+            }
+        }
+    }
+}
