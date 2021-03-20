becf48af67eff8b9926c4d793b710913f57d5dad
miranbesen <57726541+miranbesen@users.noreply.github.com>
2021-03-20 21:41:05 +0300

    Add files via upload

---
+++ WebTabanliWinProg_HW_1_HesapMakinesi/HesapMakinesi.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HesapMakinesi.aspx.cs" Inherits="WebTabanliWinProg_HW_1_HesapMakinesi.HesapMakinesi" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 417px">
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Height="39px" OnTextChanged="TextBox1_TextChanged" style="margin-left: 51px; margin-bottom: 8px" Width="255px"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Visible="False"></asp:Label>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Height="33px" OnClick="Button1_Click" style="margin-left: 52px" Text="7" Width="40px" />
+&nbsp;<asp:Button ID="Button2" runat="server" Height="32px" OnClick="Button2_Click" style="margin-left: 25px; margin-right: 0px" Text="8" Width="40px" />
+&nbsp;<asp:Button ID="Button3" runat="server" Height="31px" OnClick="Button3_Click" style="margin-left: 25px; margin-right: 0px" Text="9" Width="40px" />
+&nbsp;&nbsp; <asp:Button ID="Button4" runat="server" Height="33px" OnClick="Button4_Click" style="margin-left: 25px; margin-right: 0px" Text="/" Width="40px" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button5" runat="server" Height="38px" OnClick="Button5_Click" style="margin-left: 52px" Text="4" Width="40px" />
+&nbsp;&nbsp;
+            <asp:Button ID="Button6" runat="server" Height="34px" OnClick="Button6_Click" style="margin-left: 17px" Text="5" Width="37px" />
+&nbsp;<asp:Button ID="Button7" runat="server" Height="30px" OnClick="Button7_Click" style="margin-left: 30px; margin-top: 0px;" Text="6" Width="37px" />
+&nbsp;<asp:Button ID="Button8" runat="server" Height="33px" OnClick="Button8_Click" style="margin-left: 35px" Text="*" Width="40px" />
+        </p>
+        <p style="margin-left: 40px">
+            <asp:Button ID="Button9" runat="server" Height="35px" OnClick="Button9_Click" style="margin-left: 13px" Text="1" Width="40px" />
+            <asp:Button ID="Button10" runat="server" Height="31px" OnClick="Button10_Click" style="margin-left: 31px" Text="2" Width="40px" />
+            <asp:Button ID="Button11" runat="server" Height="33px" OnClick="Button11_Click" style="margin-left: 28px" Text="3" Width="40px" />
+            <asp:Button ID="Button12" runat="server" Height="33px" OnClick="Button12_Click" style="margin-left: 41px" Text="-" Width="40px" />
+        </p>
+        <p style="margin-left: 40px">
+            <asp:Button ID="Button13" runat="server" Height="34px" OnClick="Button13_Click" style="margin-left: 13px" Text="0" Width="36px" />
+            <asp:Button ID="Button14" runat="server" Height="34px" OnClick="Button14_Click" style="margin-left: 32px" Text="+" Width="40px" />
+            <asp:Button ID="Button15" runat="server" Height="33px" OnClick="Button15_Click" style="margin-left: 34px" Text="CLC" Width="40px" />
+            <asp:Button ID="Button16" runat="server" Height="33px" OnClick="Button16_Click" style="margin-left: 39px" Text="=" Width="39px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebTabanliWinProg_HW_1_HesapMakinesi/HesapMakinesi.aspx.cs
@@ -0,0 +1,590 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebTabanliWinProg_HW_1_HesapMakinesi
+{
+    public partial class HesapMakinesi : System.Web.UI.Page
+    {
+        static int deg1, deg2;
+        static Int64 fsonuc;
+        static string islem;
+        static int sayac=0;
+        //static int sayac2=0; //sonuca gore deg1 e atama işlemi yapılırsa.
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                TextBox1.Text = "0";
+                
+            }
+        
+
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "5";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "5";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "5";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "5";
+                }
+
+                if (islem != "")
+                {
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "5";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "5";
+                    }
+                }
+
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "0";
+            deg1 = 0;
+            deg2 = 0;
+            islem = "";
+            fsonuc = 0;
+            sayac = 0;
+            Label1.Text = "";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "9";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "9";
+
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "9";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "9";
+
+                }
+
+                if (islem != "")
+                {
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "9";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "9";
+                    }
+                }
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "4";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "4";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "4";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "4";
+                }
+
+                if (islem != "")
+                {
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "4";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "4";
+                    }
+                }
+            }
+        
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "6";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "6";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "6";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "6";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "6";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "6";
+                    }
+                }
+            }
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {  
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "1";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "1";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "1";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "1";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "1";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "1";
+                    }
+                }
+            }
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "2";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "2";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "2";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "2";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "2";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "2";
+                    }
+                }
+            }
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "3";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "3";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "3";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "3";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "3";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "3";
+                    }
+                }
+
+            }
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "0";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "0";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "0";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "0";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "0";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "0";
+                    }
+                }
+
+            }
+        }
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            deg2 = Int32.Parse(Label1.Text);
+
+            switch (islem)
+            {
+                case "/":
+                    fsonuc = deg1 / deg2;
+                    TextBox1.Text = fsonuc.ToString();
+                    Label1.Text = "";
+                    islem = "";
+                    break;
+                case "*":
+                    fsonuc = deg1 * deg2;
+                    TextBox1.Text = fsonuc.ToString();
+                    Label1.Text = "";
+                    islem = "";
+                    break;
+                case "+":
+                    fsonuc = deg1 + deg2;
+                    TextBox1.Text = fsonuc.ToString();
+                    Label1.Text = "";
+                    islem = "";
+                    break;
+                case "-":
+                    fsonuc = deg1 - deg2;
+                    TextBox1.Text = fsonuc.ToString();
+                    Label1.Text = "";
+                    islem = "";
+                    break;
+                default:
+                    TextBox1.Text = "ERROR";
+                    break;
+            }
+
+
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e) // bolme islemi butonu
+        {
+            try
+            {
+                Label2.Text = "";
+                deg1 = Int32.Parse(TextBox1.Text);
+
+                islem = "/";
+                TextBox1.Text = TextBox1.Text + islem;
+                sayac++;
+                //TextBox1.Text = "0";
+            }
+            catch
+            {
+       
+                Label2.Text = "DEGER COK BUYUK, LUTFEN DAHA KUCUK SAYILAR GİRİNİZ!!!";
+                TextBox1.Text = "0";
+                deg1 = 0;
+                deg2 = 0;
+                islem = "";
+                fsonuc = 0;
+                sayac = 0;
+                Label1.Text = "";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) 
+        {
+            if (sayac == 0)
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "7";
+                }
+                else
+
+                {
+                    TextBox1.Text = TextBox1.Text + "7";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "7";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "7";
+                }
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "7";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "7";
+                    }
+                }
+
+            }
+            
+        }
+
+        protected void Button8_Click(object sender, EventArgs e) //carpim islemi butonu
+        {
+            try
+            {
+                Label2.Text = "";
+                deg1 = Int32.Parse(TextBox1.Text);
+                islem = "*";
+                TextBox1.Text = TextBox1.Text + islem;
+                sayac++;
+                //TextBox1.Text = "0";
+            }
+
+            catch
+            {
+                Label2.Text = "DEGER COK BUYUK, LUTFEN DAHA KUCUK SAYILAR GİRİNİZ!!!";
+                TextBox1.Text = "0";
+                deg1 = 0;
+                deg2 = 0;
+                islem = "";
+                fsonuc = 0;
+                sayac = 0;
+                Label1.Text = "";
+            }
+        }
+
+        protected void Button12_Click(object sender, EventArgs e) //fark islemi butonu
+        {
+            try
+            {
+                Label2.Text = "";
+                deg1 = Int32.Parse(TextBox1.Text);
+                islem = "-";
+                TextBox1.Text = TextBox1.Text + islem;
+                sayac++;
+                //TextBox1.Text = "0";
+            }
+
+            catch
+            {
+                Label2.Text = "DEGER COK BUYUK, LUTFEN DAHA KUCUK SAYILAR GİRİNİZ!!!";
+                TextBox1.Text = "0";
+                deg1 = 0;
+                deg2 = 0;
+                islem = "";
+                fsonuc = 0;
+                sayac = 0;
+                Label1.Text = "";
+            }
+        }
+
+        protected void Button14_Click(object sender, EventArgs e) //toplama islemi butonu
+        {
+            try
+            {
+                Label2.Text = "";
+                deg1 = Int32.Parse(TextBox1.Text);
+                islem = "+";
+                TextBox1.Text = TextBox1.Text + islem;
+                sayac++;
+                //TextBox1.Text = "0";
+            }
+            catch
+            {
+                Label2.Text = "DEGER COK BUYUK, LUTFEN DAHA KUCUK SAYILAR GİRİNİZ!!!";
+                TextBox1.Text = "0";
+                deg1 = 0;
+                deg2 = 0;
+                islem = "";
+                fsonuc = 0;
+                sayac = 0;
+                Label1.Text = "";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "8";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "8";
+                }
+            }
+            else
+            {
+                if (TextBox1.Text == "0")
+                {
+                    TextBox1.Text = "8";
+                }
+                else
+                {
+                    TextBox1.Text = TextBox1.Text + "8";
+                }
+
+                if (islem != "")
+                {
+
+                    if (Label1.Text == "")
+                    {
+                        Label1.Text = "8";
+                    }
+                    else
+                    {
+                        Label1.Text = Label1.Text + "8";
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
